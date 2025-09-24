2b21fd1b3ef8106aa75d64dbbb2a216aeed20dc0
emanuel.marques <emanuel.marques@caju.com.br>
2025-09-24 20:57:01 -0300

    refactor: adjust og:image preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,13 +31,13 @@ export default function RootLayout({
         <title>Chá da Olívia 🎉</title>
         <meta property="og:title" content="Chá da Olívia 🎉" />
         <meta property="og:description" content="Contamos com a sua presença para comemorar a chegada da nossa princesinha!" />
-        <meta property="og:image" content="https://cha-baby-olivia.vercel.app/LogoInicial.png" />
+        <meta property="og:image" content="https://cha-baby-olivia.vercel.app/preview.jpg" />
         <meta property="og:url" content="https://cha-baby-olivia.vercel.app" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Chá da Olívia 🎉" />
         <meta name="twitter:description" content="Contamos com a sua presença para comemorar a chegada da nossa princesinha!" />
-        <meta name="twitter:image" content="https://cha-baby-olivia.vercel.app/LogoInicial.png" />
+        <meta name="twitter:image" content="https://cha-baby-olivia.vercel.app/preview.jpg" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
